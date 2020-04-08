34f11f8b47d58a92bfbce651c4da1802a12727b4
moms-restraunt-guide <63366727+moms-restraunt-guide@users.noreply.github.com>
2020-04-08 15:50:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //main.js
-import restrData from 'restraunt-data.js';
     const date = new Date();
     const dateDayId = date.getDay();
     const shuffle = function (array) {
@@ -37,7 +36,57 @@ import restrData from 'restraunt-data.js';
     const table = new Vue({
       el: '#table',
       data: {
-        restraunts: restrData // from restraunts-data.js
+        restraunts: [
+			 {
+			   "name": "Wendy's",
+			   "location": "Benton",
+			   "deal": "4 for $4",
+			   "dow": "Everyday",
+			   "dow_id": 8
+			 },
+			 {
+			   "name": "Red Robin",
+			   "location": "Benton",
+			   "deal": "$1.99 Kid's meal",
+			   "dow": "Tuesday",
+			   "dow_id": 2
+			 },
+			 {
+			   "name": "Newk's",
+			   "location": "Bryant",
+			   "deal": "$1.99 Kid's meal w/ purchase of adult meal",
+			   "dow": "Everyday",
+			   "dow_id": 8
+			 },
+			 {
+			   "name": "Dickies",
+			   "location": "Bryant",
+			   "deal": "Free Kid's meal w/ purchase of adult meal",
+			   "dow": "Sunday",
+			   "dow_id": 0
+			 },
+			 {
+			   "name": "Burger King",
+			   "location": " All",
+			   "deal": " $1 8pc Chicken Nuggets",
+			   "dow": " Everyday",
+			   "dow_id": 8
+			 },
+			 {
+			   "name": "Burger King",
+			   "location": " All",
+			   "deal": " 5 for $4",
+			   "dow": " Everyday",
+			   "dow_id": 8
+			 },
+			 {
+			   "name": "McDonald's",
+			   "location": " Bryant",
+			   "deal": " 4 for $4",
+			   "dow": " Everyday",
+			   "dow_id": 8
+			 }
+	    ]
       },
       computed: {
           dealList() {
