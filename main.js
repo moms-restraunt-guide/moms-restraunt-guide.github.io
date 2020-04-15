dba39e4ca63d87f63c179610532db16ab89ad78e
moms-restraunt-guide <63366727+moms-restraunt-guide@users.noreply.github.com>
2020-04-15 11:49:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//main.js
+// main.js
+
     const date = new Date();
     const dateDayId = date.getDay();
     const shuffle = function (array) {
@@ -22,84 +23,101 @@
 
     };
 
-	function sortByProperty(property){  
-	   return function(a,b){  
-    	  if(a[property] > b[property])  
-    	     return 1;  
-    	  else if(a[property] < b[property])  
-    	     return -1;  
-  
-    	  return 0;  
-   		}  
-	}
+    const restrauntData = [
+		  {
+			"name": "Wendy's",
+			"location": "Benton",
+			"deal": "$4 for 4",
+			"dow": "Everyday",
+            "dow_id": 8
+		  },
+		  {
+			"name": "Red Robin",
+			"location": "Benton",
+			"deal": "$0.99 Kid's meal",
+			"dow": "Tuesday",
+            "dow_id": 2
+		  },
+		  { 
+			"name": "Newk's",
+			"location": "Bryant",
+			"deal": "$1.99 Kid's meal w/ purchase of adult meal",
+			"dow": "Everyday",
+            "dow_id": 8
+		  },
+		  {
+			"name": "Dickies",
+			"location": "Bryant",
+			"deal": "Free Kid's meal w/ purchase of adult meal",
+			"dow": "Sunday",
+            "dow_id": 0
+		  },
+		  {
+			"name": "Moe's",
+			"location": "Bryant",
+			"deal": "Free Kid's meal w/ purchase of adult meal",
+			"dow": "Sunday",
+            "dow_id": 0
+		  },
+		  {
+			"name": "McAllister's Deli",
+			"location": "Bryant",
+			"deal": "Free Kid's meal w/ purchase of adult meal",
+			"dow": "Monday",
+            "dow_id": 1
+		  },
+		  {
+			"name": "Taco Bueno",
+			"location": "Bryant",
+			"deal": "$2.99 Taco Platter",
+			"dow": "Tuesday",
+            "dow_id": 2
+		  },   
+		  {
+			"name": "Firehouse Subs",
+			"location": "Bryant",
+			"deal": "Free Kid's meal w/ purchase of adult meal",
+			"dow": "Wednesday",
+            "dow_id": 3
+		  },
+		  {
+			"name": "Woodfire Grill",
+			"location": "Bryant",
+			"deal": "1/2 price kid's meal after 4:00p",
+			"dow": "Wednesday",
+            "dow_id": 3
+		  },
+		  {
+			"name": "Taco Bueno",
+			"location": "Bryant",
+			"deal": "$0.59 party tacos",
+			"dow": "Thursday",
+            "dow_id": 4
+		  },
+		  {
+			"name": "Arby's",
+			"location": "Bryant",
+			"deal": "Happy Hour from 2:00p to 5:00p",
+			"dow": "Everyday",
+            "dow_id": 8
+		  },
+        ]
+
+
+
 
     const table = new Vue({
       el: '#table',
       data: {
-        restraunts: [
-			 {
-			   "name": "Wendy's",
-			   "location": "Benton",
-			   "deal": "4 for $4",
-			   "dow": "Everyday",
-			   "dow_id": 8
-			 },
-			 {
-			   "name": "Red Robin",
-			   "location": "Benton",
-			   "deal": "$1.99 Kid's meal",
-			   "dow": "Tuesday",
-			   "dow_id": 2
-			 },
-			 {
-			   "name": "Newk's",
-			   "location": "Bryant",
-			   "deal": "$1.99 Kid's meal w/ purchase of adult meal",
-			   "dow": "Everyday",
-			   "dow_id": 8
-			 },
-			 {
-			   "name": "Dickies",
-			   "location": "Bryant",
-			   "deal": "Free Kid's meal w/ purchase of adult meal",
-			   "dow": "Sunday",
-			   "dow_id": 0
-			 },
-			 {
-			   "name": "Burger King",
-			   "location": " All",
-			   "deal": " $1 8pc Chicken Nuggets",
-			   "dow": " Everyday",
-			   "dow_id": 8
-			 },
-			 {
-			   "name": "Burger King",
-			   "location": " All",
-			   "deal": " 5 for $4",
-			   "dow": " Everyday",
-			   "dow_id": 8
-			 },
-			 {
-			   "name": "McDonald's",
-			   "location": " Bryant",
-			   "deal": " 4 for $4",
-			   "dow": " Everyday",
-			   "dow_id": 8
-			 }
-	    ]
+        restrauntData
       },
       computed: {
           dealList() {
-            return shuffle(this.restraunts.filter(item => item.dow_id === dateDayId || item.dow_id === 8));
+            return shuffle(this.restrauntData.filter(item => item.dow_id === dateDayId || item.dow_id === 8));
           },
-          randomItem() {
-            return shuffle(this.restraunts.filter(item => item.dow_id === dateDayId || item.dow_id === 8)).slice(0,1);
-          },
-          restrauntsSorted() {
-		    return this.restraunts.sort(sortByProperty("dow_id")); //sort according to dow_id
-		  },
           today(){
             return Date();
           }
       }
     })    
+
